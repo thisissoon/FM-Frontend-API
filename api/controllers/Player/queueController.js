d25f6a0b245f760606b5fac12b50c4f346d5f1b0
James Warren <mail@jamesjwarren.com>
2015-08-10 17:19:31 +0100

    move FM API wrapper to service

--- api/controllers/Player/queueController.js
+++ api/controllers/Player/queueController.js
@@ -5,25 +5,9 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-var rest = require('restler');
 
-var FM = rest.service(function(u, p) {
-  this.defaults.username = u;
-  this.defaults.password = p;
-}, {
-  baseURL: 'http://localdocker:5000'
-}, {
-  update: function(track) {
-    return this.put('/player/queue', track);
-  }
-});
 
 module.exports = {
-  find: function (req, res) {
-    var fm = new FM('', '');
-    fm.get(req.url).on('complete', function (response) {
-      res.send(response);
-    });
-  }
+
 };
 
