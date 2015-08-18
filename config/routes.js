563c37aad6d38cb2cdaa96260af0608a4b54d398
Edward Opare-Aryee <ed@thisissoon.com>
2015-08-18 16:39:03 +0100

    change name of custom blueprint to avoid conflicts

--- config/routes.js
+++ config/routes.js
@@ -47,10 +47,10 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
-  'GET /api/player/current':    { model: 'player/current',    blueprint: 'findone' },
-  'GET /api/player/mute':       { model: 'player/mute',       blueprint: 'findone' },
-  'GET /api/player/volume':     { model: 'player/volume',     blueprint: 'findone' },
-  'GET /api/player/queue/meta': { model: 'player/queue/meta', blueprint: 'findone' }
+  'GET /api/player/current':    { model: 'player/current',    blueprint: 'findasobject' },
+  'GET /api/player/mute':       { model: 'player/mute',       blueprint: 'findasobject' },
+  'GET /api/player/volume':     { model: 'player/volume',     blueprint: 'findasobject' },
+  'GET /api/player/queue/meta': { model: 'player/queue/meta', blueprint: 'findasobject' }
 
 
 };
