5e6d77df785fce6a2daf70d038b852ebfd0ffbd8
James Warren <mail@jamesjwarren.com>
2015-08-19 10:27:01 +0100

    player/stats endpoint

---
+++ api/controllers/Player/statsController.js
@@ -0,0 +1,11 @@
+/**
+ * Player/statsController
+ *
+ * @description :: Server-side logic for managing player/stats
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
+
