bfe384240c962eb3eb655062a1b7ede877c85be9
James Warren <mail@jamesjwarren.com>
2015-08-19 10:19:35 +0100

    tracks endpoint

---
+++ api/controllers/TracksController.js
@@ -0,0 +1,11 @@
+/**
+ * TracksController
+ *
+ * @description :: Server-side logic for managing tracks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
+
